df766e61513ee02783af4c5423872e69b002d24d
crazyne55 <95332356+crazyne55@users.noreply.github.com>
2023-03-24 19:24:45 -0700

    had to use some tools to verify the numbers were right.

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,29 @@ public class Main {
 
     public static void main(String[] args) {
         // Create a scanner object
+        Scanner s = new Scanner(System.in);
 
         // Write a loop that will ask the user to enter a file path to gather stats on,
         // and continue until "Q" is entered.
-
+        String fileStr;
+        while(true) {
+            System.out.print("Please enter the file you would like to check (enter \"q\" to exit). ");
+            fileStr = s.nextLine();
+            if (fileStr.toLowerCase().equals("q")) {
+                break;
+            }
             // Reference Java-Assignment-003 to see how to use the java.nio libraries to turn a String path into a File
+            File file = Paths.get(fileStr).toFile();
 
             // Ask the user if they would like to skip whitespace
+            System.out.print("Include whitespace? (yes/NO)");
+            String yn = s.nextLine();
 
             // Create a variable called skipWs that stores the user's response as a boolean
+            boolean skipWs = true;
+            if(yn.toLowerCase().equals("y") || yn.toLowerCase().equals("yes")) {
+                skipWs = false;
+            }
 
             /*
              * Within this try/catch block, which is used to handle possible errors thrown by the code in the try block,
@@ -24,8 +38,10 @@ public class Main {
              */
             try {
                 // You will need to create a FileStats object by passing it the File object and your skipWs variable as args
+                FileStats fs = new FileStats(file, skipWs);
 
                 // You will need to call the fs.read method, which you need to implement!
+                fs.read();
 
                 /*
                  * You will access the FileStats object's getter methods to get the file's line, word, character count and
@@ -33,9 +49,10 @@ public class Main {
                  *
                  * Stats: lines - 6, words - 46, chars - 237 /path/to/file/fileName.txt
                  */
+                System.out.printf("Stats: lines - %s, words - %s, chars - %s %s%n",fs.getNumLines(),fs.getNumWords(),fs.getNumChars(),fs.getFileName());
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
-
+        }
     }
 }
